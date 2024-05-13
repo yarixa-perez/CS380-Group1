01ef04ebcd3bafef02f698913cb572caa067b465
Yarixa Perez <perezyari@cwu.edu>
2024-05-13 00:50:04 -0700

    Added Sandwich.java class file

---
+++ Sandwich_Shop_Point_Of_Sale/Sandwich_Shop_Point_Of_Sale/src/Sandwich.java
@@ -0,0 +1,13 @@
+
+public class Sandwich extends Item {
+
+    /**
+     * Constructor for a Sandwich item
+     * @param type The type of product of a Sandwich item
+     * @param name The name of a Sandwich item
+     * @param price The price of a Sandwich item
+     */
+    public Sandwich(ProductType type, String name, float price) {
+        super(type, name, price);
+    }
+}
