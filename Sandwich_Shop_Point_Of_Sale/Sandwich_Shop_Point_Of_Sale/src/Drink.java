f1ecad3459d53a3c4596ba25ec66bba9e2595ce5
Yarixa Perez <perezyari@cwu.edu>
2024-05-13 00:47:20 -0700

    Added Drink.java class file

---
+++ Sandwich_Shop_Point_Of_Sale/Sandwich_Shop_Point_Of_Sale/src/Drink.java
@@ -0,0 +1,13 @@
+
+public class Drink extends Item {
+
+    /**
+     * Constructor for a Drink item
+     * @param type The type of product of a Drink item
+     * @param name The name of a Drink item
+     * @param price The price of a Drink item
+     */
+    public Drink(ProductType type, String name, float price) {
+        super(type, name, price);
+    }
+}
