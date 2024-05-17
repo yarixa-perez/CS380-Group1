0ecfc5785954cf527594ce09b7f3b051731105fd
Ruby Rosales <rubyrosales2004@outlook.com>
2024-05-17 11:41:07 -0700

    Added employee class.

---
+++ Sandwich_Shop_Point_Of_Sale/Sandwich_Shop_Point_Of_Sale/src/Employee.java
@@ -0,0 +1,21 @@
+public class Employee extends Person{
+    protected int employeeNumber;
+    protected String position;
+    protected float wage;
+
+    /**
+     * The constructor for the employee class
+     * @param firstName First name of the employee
+     * @param lastName Last name of the employee
+     * @param DOB Date of birth of the employee
+     * @param employeeNumber Employee number of the employee
+     * @param position Position of the employee
+     * @param wage Employees wage
+     */
+    public Employee(String firstName, String lastName, Date DOB, int employeeNumber, String position, float wage) {
+        super(firstName, lastName, DOB);
+        this.employeeNumber = employeeNumber;
+        this.position = position;
+        this.wage = wage;
+    }
+}
