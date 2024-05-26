57b5b56aa2c78ddc3c7d230f7fe5ec8dd75f9ee7
Mohammed Mohideen <mohideenabm@cwu.edu>
2024-05-26 01:44:39 -0700

    Finished edit order method and added comments

--- Sandwich_Shop_Point_Of_Sale/Sandwich_Shop_Point_Of_Sale/src/OrderManager.java
+++ Sandwich_Shop_Point_Of_Sale/Sandwich_Shop_Point_Of_Sale/src/OrderManager.java
@@ -5,22 +5,49 @@ import java.util.ArrayList;
 
 public class OrderManager{
 
+   // array list of orders
    private ArrayList<Order> orderList;
    
+   // constructor
    public OrderManager(){
       orderList = new ArrayList<Order>();
    }
    
+    /**
+     * Adds an order to the order list
+     * @param order Order to add to list.
+     */
    public void addOrder(Order order) {
       orderList.add(order);
    }
-
+   
+    /**
+     * Removes an order from the order list
+     * @param order Order to remove from the list.
+     */
    public void removeOrder(Order order) {
       orderList.remove(order);
    }
    
-   public void editOrder(Order order) {}
+    /**
+     * Replaces an order in the list with an updated order
+     * @param order Order to update
+     */
+   public void editOrder(Order updatedOrder) {
+      for (int i = 0; i < orderList.size(); i++) {
+         Order currentOrder = orderList.get(i);
+         if (currentOrder.getOrderNumber() == updatedOrder.getOrderNumber()) {
+            orderList.set(i, updatedOrder);
+            return;
+         }
+      }
+   }
    
+    /**
+     * Accesses an order from the list
+     * @return Returns order details
+     * @param order Order to access
+     */
    public Order getOrder(int orderNumber) {
       for (Order order : orderList) {
          if (order.getOrderNumber() == orderNumber) {
