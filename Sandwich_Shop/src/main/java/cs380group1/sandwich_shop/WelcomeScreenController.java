0b454b0489300955da5f994fa9c803b6b98cbb25
Yarixa Perez <perezyari@cwu.edu>
2024-05-28 23:27:44 -0700

    Modified WelcomeScreenController.java file

--- Sandwich_Shop/src/main/java/cs380group1/sandwich_shop/WelcomeScreenController.java
+++ Sandwich_Shop/src/main/java/cs380group1/sandwich_shop/WelcomeScreenController.java
@@ -2,7 +2,12 @@ package cs380group1.sandwich_shop;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class WelcomeScreenController {
 
@@ -11,7 +16,17 @@ public class WelcomeScreenController {
 
     @FXML
     void Start(ActionEvent event) {
-
+        loadScene("SignInScreen.fxml", event);
     }
 
+    private void loadScene(String fxmlFile, ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(fxmlFile));
+            Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
