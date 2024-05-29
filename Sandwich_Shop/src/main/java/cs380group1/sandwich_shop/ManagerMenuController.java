65540b30379b135ce9986a3de125053c44cf3d11
Yarixa Perez <perezyari@cwu.edu>
2024-05-28 17:37:36 -0700

    Added ManagerMenuController.java class file

---
+++ Sandwich_Shop/src/main/java/cs380group1/sandwich_shop/ManagerMenuController.java
@@ -0,0 +1,40 @@
+package cs380group1.sandwich_shop;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ManagerMenuController {
+
+    @FXML
+    private Button EmployeeMenuButton;
+    private Button ViewDailyReportButton;
+    private Button ViewEmployeeInfoButton;
+    private Button ViewStockButton;
+    private Button ViewTodaysOrdersButton;
+
+    @FXML
+    void GoToEmployeeMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ViewDailyReport(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ViewEmployeeInfo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ViewStock(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ViewTodaysOrders(ActionEvent event) {
+
+    }
+}
