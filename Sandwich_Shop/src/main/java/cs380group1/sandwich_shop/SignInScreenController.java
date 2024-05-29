e7bc779da3c5a612e63459d79ac6d9693e35a543
Yarixa Perez <perezyari@cwu.edu>
2024-05-28 23:25:17 -0700

    Added SignInController.java class file

---
+++ Sandwich_Shop/src/main/java/cs380group1/sandwich_shop/SignInScreenController.java
@@ -0,0 +1,20 @@
+package cs380group1.sandwich_shop;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+
+public class SignInScreenController {
+
+    @FXML
+    private Button LoginButton;
+
+    @FXML
+    private PasswordField PasswordField;
+
+    @FXML
+    void Login(ActionEvent event) {
+
+    }
+}
