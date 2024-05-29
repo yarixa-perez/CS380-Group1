a80431a879d7b04dfb8aa47799488974f5ddd66c
Yarixa Perez <perezyari@cwu.edu>
2024-05-28 19:43:07 -0700

    Modified ManagerMenuController.java class file

--- Sandwich_Shop/src/main/java/cs380group1/sandwich_shop/ManagerMenuController.java
+++ Sandwich_Shop/src/main/java/cs380group1/sandwich_shop/ManagerMenuController.java
@@ -3,6 +3,11 @@ package cs380group1.sandwich_shop;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class ManagerMenuController {
 
@@ -15,26 +20,37 @@ public class ManagerMenuController {
 
     @FXML
     void GoToEmployeeMenu(ActionEvent event) {
-
+        loadScene("EmployeeMenu.fxml", event);
     }
 
     @FXML
     void ViewDailyReport(ActionEvent event) {
-
+        loadScene("", event);
     }
 
     @FXML
     void ViewEmployeeInfo(ActionEvent event) {
-
+        loadScene("ViewEmployeeInfoTable.fxml", event);
     }
 
     @FXML
     void ViewStock(ActionEvent event) {
-
+        loadScene("ViewStock.fxml", event);
     }
 
     @FXML
     void ViewTodaysOrders(ActionEvent event) {
+        loadScene("ViewOrders.fxml", event);
+    }
 
+    private void loadScene(String fxmlFile, ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(fxmlFile));
+            Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
